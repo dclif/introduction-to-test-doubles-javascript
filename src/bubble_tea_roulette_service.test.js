dea1e1f871f2b7b8e27d076bd402ce1ef33b4caa
Daniel <65896341+dclif@users.noreply.github.com>
2022-03-08 16:02:13 +0000

    Test for PEACHICETEA
    Test for PEACHICETEA

--- src/bubble_tea_roulette_service.test.js
+++ src/bubble_tea_roulette_service.test.js
@@ -14,3 +14,19 @@ test('should generate random bubble tea', () => {
   // Restore the default Math.random
   jest.spyOn(global.Math, 'random').mockRestore();
 });
+
+test('should generate random bubble tea', () => {
+  // Arrange
+  // Stub to calls to Math.random to return 0.2
+  jest.spyOn(global.Math, 'random').mockReturnValue(0.6);
+
+  // Act
+  const bubbleTea = getRandomBubbleTeaType();
+
+  // Assert
+  expect(bubbleTea).toBe('PEACHICETEA');
+
+  // Restore the default Math.random
+  jest.spyOn(global.Math, 'random').mockRestore();
+});
+
